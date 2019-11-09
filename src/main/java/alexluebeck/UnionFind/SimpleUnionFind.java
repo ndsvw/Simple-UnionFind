9d2aa6d01528bff9d5b4f0043cfbfa260d1f2e41
Alex Lübeck <ndsvw@gmx.de>
2019-11-09 17:04:29 +0100

    implemented SimpleUnionFind

---
+++ src/main/java/alexluebeck/UnionFind/SimpleUnionFind.java
@@ -0,0 +1,48 @@
+package alexluebeck.UnionFind;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class SimpleUnionFind implements UnionFind {
+	private ArrayList<Integer> inWhichSet;
+	private ArrayList<HashSet<Integer>> sets;
+
+	public SimpleUnionFind(int size) {
+		sets = new ArrayList<HashSet<Integer>>(size);
+		inWhichSet = new ArrayList<Integer>(size);
+		for (int i = 0; i < size; i++) {
+			HashSet<Integer> tmpHashSet = new HashSet<Integer>();
+			tmpHashSet.add(i);
+			sets.add(tmpHashSet);
+			inWhichSet.add(i);
+		}
+	}
+
+	public int find(int i) {
+		return inWhichSet.get(i);
+	}
+
+	public void union(int y, int z) {
+		int size1 = sets.get(inWhichSet.get(y)).size();
+		int size2 = sets.get(inWhichSet.get(z)).size();
+
+		if (size1 >= size2) {
+			HashSet<Integer> source = sets.get(inWhichSet.get(z));
+			HashSet<Integer> dest = sets.get(inWhichSet.get(y));
+			for (Integer x : source) {
+				dest.add(x);
+				inWhichSet.set(x, inWhichSet.get(y));
+			}
+			source.clear();
+		} else {
+			HashSet<Integer> source = sets.get(inWhichSet.get(y));
+			HashSet<Integer> dest = sets.get(inWhichSet.get(z));
+			for (Integer x : source) {
+				dest.add(x);
+				inWhichSet.set(x, inWhichSet.get(z));
+			}
+			source.clear();
+		}
+	}
+
+}
